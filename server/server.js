2b73a9d5ad413c15dfe8843278e21e688df367ff
jnrkoby3-ai <jnrkoby3@gmail.com>
2026-01-11 14:17:39 -0800

    added changes made here to shutting down property

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Shutting down gracefully...');
+  console.log('changes made here Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
